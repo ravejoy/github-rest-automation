9f390fa66d4c1c9dc608d94ff053d12c6d43a6c9
Nazarii Tsyhaniuk <nazariyt@gmail.com>
2025-10-19 00:04:51 +0100

    test: add sanity test to verify framework bootstraps

---
+++ src/test/java/com/ravejoy/github/SanityTest.java
@@ -0,0 +1,12 @@
+package com.ravejoy.github;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class SanityTest {
+
+    @Test
+    void frameworkBootstraps() {
+        assertThat(true).isTrue();
+    }
+}
