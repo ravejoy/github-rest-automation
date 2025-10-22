8217dd41dd71c23ad96036a0422bba7386a03509
Nazarii Tsyhaniuk <nazariyt@gmail.com>
2025-10-22 02:03:17 +0100

    chore(test): move TestConfig to support package (test helper, not a test case)

--- src/test/java/com/ravejoy/github/support/TestConfig.java
+++ src/test/java/com/ravejoy/github/support/TestConfig.java
@@ -1,4 +1,6 @@
-package com.ravejoy.github.config;
+package com.ravejoy.github.support;
+
+import com.ravejoy.github.config.ConfigLoader;
 
 public final class TestConfig {
   private static final ConfigLoader L = new ConfigLoader("test.properties");
